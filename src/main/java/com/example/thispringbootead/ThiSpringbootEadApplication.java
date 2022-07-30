22bf6401df4ee495443aa3fd70b9b8fd99ed62bf
DESKTOP-0K5Q27J\uinhi <datltth2009017@fpt.edu.vn>
2022-07-30 15:13:16 +0700

    thi

---
+++ src/main/java/com/example/thispringbootead/ThiSpringbootEadApplication.java
@@ -0,0 +1,13 @@
+package com.example.thispringbootead;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThiSpringbootEadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThiSpringbootEadApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/thispringbootead/ThiSpringbootEadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.thispringbootead;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThiSpringbootEadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
